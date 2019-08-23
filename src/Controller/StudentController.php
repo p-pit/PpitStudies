b85bf5c9257ddb9932435bc79e3ec9da295c6f58
bruno <bruno@MacBook-Pro-de-Bruno.local>
2019-08-23 14:16:27 +0200

    Fixed global average

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -179,7 +179,7 @@ class StudentController extends AbstractActionController
     	$school_periods = $place->getConfig('school_periods');
     	$current_school_period = 'Q1'; //$context->getCurrentPeriod($school_periods);
 		$cursor = NoteLink::getList('report', ['category' => 'evaluation', 'subject' => 'global', 'school_year' => $current_school_year, 'school_period' => $current_school_period, 'account_id' => $profile->id], 'id', 'ASC', $mode = 'search');
-		foreach ($cursor as $report) $averageNote = current($cursor); // Should be unique but to keep only the last one
+		foreach ($cursor as $report) $averageNote = $report; // Should be unique but to keep only the last one
 		$global_average = ($averageNote) ? $averageNote->value : null;
 		
 		$view = new ViewModel(array(
