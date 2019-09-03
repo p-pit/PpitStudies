29366a23b301ace3b01b40dc0de9823c77872995
bruno <bruno@MacBook-Pro-de-Bruno.local>
2019-09-03 09:18:26 +0200

    Attendance list

---
+++ config/event_message_p_pit_studies.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * PpitCore V1.0 (https://github.com/p-pit/PpitCore)
+ *
+ * @link      https://github.com/p-pit/PpitCore
+ * @copyright Copyright (c) 2016 Bruno Lartillot
+ * @license   https://github.com/p-pit/PpitCore/blob/master/license.txt GNU-GPL license
+ */
+
+define('EVENT_MESSAGE_P_PIT_STUDIES', [
+
+	'event/message/p-pit-studies/attendance_sheet' => [
+		'name' => ['default' => 'Attendance sheet', 'fr_FR' => 'Feuille d’émargement'],
+		'filters' => ['groups', 'property_7'],
+		'style' => ['default' => '
+			<style>
+	    	table {
+	    		border-collapse: collapse;
+			}
+			table, th, td {
+	    		font-size: 10;
+				padding-top: 10;
+				padding-right: 5;
+				padding-bottom: 10;
+				padding-left: 5;
+			border: 1px solid black;
+			}
+			.addressee {
+				font-weight: bold;
+				font-size: 12;
+			}
+			.text-center {
+				text-align: center;
+			}
+			.text-justify{
+				text-align: justify;
+			}
+			</style>
+		'],
+		'sections' => [
+			'header' => [
+				'class' => 'header',
+				'paragraphs' => [
+				],
+			],
+			'title' => [
+				'class' => 'box-title',
+				'paragraphs' => [
+					['type' => 'h1', 'class' => 'text-center', 'label' => ['default' => 'ATTENDANCE SHEET', 'fr_FR' => 'LISTE D’ÉMARGEMENT']],
+				],
+			],
+			'body' => [
+				'class' => 'body',
+				'paragraphs' => [
+					['type' => 'br'],
+					[
+						'type' => 'p',
+						'class' => 'text-center',
+						'label' => [
+							'default' => 'Module: <strong>%s</strong> &mdash; Date: <strong>%s</strong> &mdash; Start: <strong>%s</strong> &mdash; End: <strong>%s</strong> &mdash; Duration: <strong>%s</strong>',
+							'fr_FR' => 'Module: <strong>%s</strong> &mdash; Date: <strong>%s</strong> &mdash; Début: <strong>%s</strong> &mdash; Fin: <strong>%s</strong> &mdash; Durée: <strong>%sh</strong>'
+						],
+						'params' => ['property_2', 'begin_date', 'begin_time', 'end_time', 'duration'],
+					],
+					['type' => 'br'],
+				],
+			],
+			'table' => [
+				'class' => 'table',
+				'entity' => 'account',
+				'columns' => [
+					[
+						'class' => 'text-justify',
+						'header' => ['default' => '<strong>Trainee</strong> / Hours', 'fr_FR' => '<strong>Stagiaire</strong> / Horaire'],
+						'label' => ['default' => '<strong>%s %s</strong> / from %s to %s (%s)', 'fr_FR' => '<strong>%s %s</strong> / de %s à %s (%sh)'],
+						'params' => ['account:n_first', 'account:n_last', 'begin_time', 'end_time', 'duration'],
+					],
+					[
+						'class' => 'text-justify',
+						'header' => ['default' => '<strong>Trainee’s signature</strong> / Hours', 'fr_FR' => '<strong>Signature du stagiaire</strong> / Horaire'],
+						'label' => ['default' => '&nbsp;'],
+					],
+				],
+			],
+			'footer' => [
+				'class' => 'footer',
+				'paragraphs' => [
+				],
+			],
+		],
+	],
+	
+	'event/message/p-pit-studies' => [
+		'attendance_sheet',
+	],
+]);
--- config/module.config.php
+++ config/module.config.php
@@ -2,6 +2,7 @@
 namespace PpitStudies;
 
 include('commitment_message_p_pit_studies.php');
+include('event_message_p_pit_studies.php');
 
 return array_merge(
 [
@@ -2698,7 +2699,7 @@ return array_merge(
 			'n_title_3', 'n_first_3', 'n_last_3', 'n_fn_3', 'email_3', 'tel_work_3', 'tel_cell_3', 'address_3',
 			'n_title_4', 'n_first_4', 'n_last_4', 'n_fn_4', 'email_4', 'tel_work_4', 'tel_cell_4', 'address_4',
 			'n_title_5', 'n_first_5', 'n_last_5', 'n_fn_5', 'email_5', 'tel_work_5', 'tel_cell_5', 'address_5',
-			'opening_date', 'closing_date', 'callback_date', 'first_activation_date', 'date_1', 'date_2', 'date_3', 'date_4', 'date_5', 'next_meeting_date', 'next_meeting_confirmed', 'priority', 'origine', 'contact_history',
+			'groups', 'opening_date', 'closing_date', 'callback_date', 'first_activation_date', 'date_1', 'date_2', 'date_3', 'date_4', 'date_5', 'next_meeting_date', 'next_meeting_confirmed', 'priority', 'origine', 'contact_history',
 			'default_means_of_payment', 'transfer_order_id', 'transfer_order_date', 'bank_identifier',
 			'property_1', 'property_2', 'property_3', 'property_4', 'property_5', 'property_6', 'property_7', 'property_8',
 			'property_9', 'property_10', 'property_11', 'property_12', 'property_13', 'property_14', 'property_15', 'property_16', 'property_17', 'property_18', 'property_19', 'property_20',
@@ -2724,6 +2725,7 @@ return array_merge(
 					'callback_date' => [],
 					'property_8' => [],
 					'next_meeting_date' => [],
+					'groups' => ['multiple' => true],
 					'property_7' => ['multiple' => true],
 					'property_18' => ['multiple' => true],
 					'property_6' => ['multiple' => true],
@@ -5546,5 +5548,6 @@ table.note-report tr.period {
 	),
 ],
 
-	COMMITMENT_MESSAGE_P_PIT_STUDIES
+	COMMITMENT_MESSAGE_P_PIT_STUDIES,
+	EVENT_MESSAGE_P_PIT_STUDIES
 );
