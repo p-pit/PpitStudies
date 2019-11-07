50bd43873d3288ef35ff081c108aa09b2850d4ad
bruno <bruno@macbook-pro-de-bruno.home>
2019-11-07 22:02:58 +0100

    Check average computation in stock

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -993,7 +993,7 @@ class NoteController extends AbstractActionController
 								$noteLink->value = $value;
 								$noteLink->distribution = $distribution;
 								$noteLink->audit = $audit;
-								$noteLink->update(null);
+//								$noteLink->update(null);
 							}
 						}
 					}
