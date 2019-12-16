858d4e86f4d109db112fd8e0089e2e77fe81d146
bruno <bruno@macbook-pro-de-bruno.home>
2019-12-16 19:53:03 +0100

    Fixed average computation

--- src/Model/Note.php
+++ src/Model/Note.php
@@ -301,11 +301,13 @@ class Note
     		$average = 0;
     		$globalWeight = 0;
     		foreach ($notes as $note) {
-    			$average += $note['note'] * 20 / $note['reference_value'] * $note['weight'];
-    			$globalWeight += $note['weight'];
+    			$average += $note['note'] * $note['weight'];
+    			$globalWeight += $note['reference_value'] * $note['weight'];
+/*    			$average += $note['note'] * 20 / $note['reference_value'] * $note['weight'];
+    			$globalWeight += $note['weight'];*/
     		}
     		if ($globalWeight != 0) {
-    			$average /= $globalWeight;
+    			$average /= $globalWeight * 20;
 	    		$categoryAverages[$account_id]['global'] = array('note' => $average, 'notes' => $notes);
     		}
     	}
