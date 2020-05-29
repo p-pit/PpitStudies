dad53c36bc84693c825115d171cc9faba24078ac
bruno <bruno@macbook-pro-de-bruno.home>
2020-05-29 17:00:31 +0200

    note/batchAverage

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -1410,7 +1410,7 @@ class NoteController extends AbstractActionController
 						foreach ($computedAverages[$noteLink->account_id] as $categoryId => $category) {
 							if ($categoryId != 'global') $distribution[$categoryId] = $category['note'];
 						}
-						if ((int) round($value * 100) != (int) round($noteLink->value * 100) || count($distribution) != count($noteLink->distribution)) {
+						if ((int) round($value * 100) != (int) round($noteLink->value * 100) /*|| count($distribution) != count($noteLink->distribution)*/) {
 							print_r($note->type.' Note: '.$note->id.' Link: '.$noteLink->id.' Account: ' . $noteLink->account_id . ' ' . $noteLink->n_fn.' '.$note->class.' '.$note->subject."\n");
 							print_r('New: '.$value."\n");
 							print_r($distribution);
