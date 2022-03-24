98039e2c841790bfb4a4f35b89fa6eb9919b3e8b
blartillot <lartillotb@users.noreply.github.com>
2022-03-24 16:42:59 +0100

    Bug fix

--- src/Controller/NoteLinkController.php
+++ src/Controller/NoteLinkController.php
@@ -307,7 +307,7 @@ class NoteLinkController extends AbstractActionController
 		$order = $this->params()->fromQuery('order', '-date');
 		
 		// Retrieve the teachers
-		$select = Vcard::getTable()->getSelect()->order('n_fn ASC');
+		/*$select = Vcard::getTable()->getSelect()->order('n_fn ASC');
 		$where = new Where;
 		$where->notEqualTo('status', 'deleted');
 		$where->like('roles', '%teacher%');
@@ -315,8 +315,10 @@ class NoteLinkController extends AbstractActionController
 		$cursor = Vcard::getTable()->selectWith($select);
 		$contact = null;
 		$teachers = array();
-		foreach ($cursor as $contact) $teachers[$contact->id] = $contact;
-
+		foreach ($cursor as $contact) $teachers[$contact->id] = $contact;*/
+		$cursor = Account::getListV3('teacher', ['n_fn', 'contact_1_id'], ['status' => 'active,committed,contrat_envoye,reconnect_with'], '+name');
+		$teachers = [];
+		foreach ($cursor as $teacher_id => $teacher) $teachers[$teacher['contact_1_id']] = $teacher;
 		// Retrieve the groups
 		$groups = Account::getList('group', [], null, null, ['id', 'status', 'name'], false, false, false, true);
 
