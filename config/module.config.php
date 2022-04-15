ed1a428c8fa73ee2d7b8818fba9b42d13ac19e50
blartillot <lartillotb@users.noreply.github.com>
2022-04-15 11:52:59 +0200

    event(calendar): school property

--- config/module.config.php
+++ config/module.config.php
@@ -5136,7 +5136,7 @@ table.note-report tr.period {
 		'dimensions' => array(),
 		'indicators' => array(),
 		'properties' => array(
-			'status', 'type', 'place_id', 'place_caption', 'vcard_id', 'n_fn', 'account_status', 'email_work', 'category', 'subcategory', 'identifier', 'caption', 'description',
+			'status', 'type', 'place_id', 'school', 'place_caption', 'vcard_id', 'n_fn', 'account_status', 'email_work', 'category', 'subcategory', 'identifier', 'caption', 'description',
 			'begin_date', 'end_date', 'day_of_week', 'day_of_month', 'exception_1', 'exception_2', 'exception_3', 'exception_4', 'begin_time', 'end_time', 'time_zone', 'location', 'latitude', 'longitude',
 			'value', 'comments',
 			'property_1', 'property_2', 'property_3', 'property_4', 
