c85073d23f2a4ff2f4925a44769e0ea0b3e7a0a6
AbdoulkadriBoureima <112864840+AbdoulkadriBoureima@users.noreply.github.com>
2022-10-12 13:20:18 +0200

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -5708,10 +5708,10 @@ table.note-report tr.period {
 			
 			// Student Financials
 			'to_justify' => ['position' => 'F', 'type' => 'count', 'labels' => ['default' => 'À justifier'], 'background' => '#EBF1DE', 'filter' => ['property_12' => 'to_justify']],
-			'processing' => ['position' => 'G', 'type' => 'count', 'labels' => ['default' => 'En cour de traitement'], 'background' => '#EBF1DE','filter' => ['property_12' => 'processing']],
-			'medical' => ['position' => 'H', 'type' => 'count', 'labels' => ['default' => 'Arrer maladie'], 'background' => '#EBF1DE','filter' => ['property_12' => 'medical']],
-			'enterprise' => ['position' => 'I', 'type' => 'count', 'labels' => ['default' => 'Presence en enteprise'], 'background' => '#EBF1DE','filter' => ['property_12' => 'enterprise']],
-			'unjustified' => ['position' => 'J', 'type' => 'count', 'labels' => ['default' => 'Non justifier'], 'background' => '#EBF1DE','filter' => ['property_12' => 'unjustified']],
+			'processing' => ['position' => 'G', 'type' => 'count', 'labels' => ['default' => 'En cours de traitement'], 'background' => '#EBF1DE','filter' => ['property_12' => 'processing']],
+			'medical' => ['position' => 'H', 'type' => 'count', 'labels' => ['default' => 'Arret maladie'], 'background' => '#EBF1DE','filter' => ['property_12' => 'medical']],
+			'enterprise' => ['position' => 'I', 'type' => 'count', 'labels' => ['default' => 'Présence en enteprise'], 'background' => '#EBF1DE','filter' => ['property_12' => 'enterprise']],
+			'unjustified' => ['position' => 'J', 'type' => 'count', 'labels' => ['default' => 'Non justifiée'], 'background' => '#EBF1DE','filter' => ['property_12' => 'unjustified']],
 			'other' => ['position' => 'K', 'type' => 'count', 'labels' => ['default' => 'Autre justificatif'], 'background' => '#EBF1DE','filter' => ['property_12' => 'other']],
 
 			'total_student' => [
@@ -5726,10 +5726,10 @@ table.note-report tr.period {
 			// Totals Students
 
 			'to_justify' => ['position' => 'F', 'type' => 'count', 'labels' => ['default' => 'À justifier'], 'background' => '#EBF1DE', 'filter' => ['property_12' => 'to_justify']],
-			'processing' => ['position' => 'G', 'type' => 'count', 'labels' => ['default' => 'En cour de traitement'], 'background' => '#EBF1DE','filter' => ['property_12' => 'processing']],
-			'medical' => ['position' => 'H', 'type' => 'count', 'labels' => ['default' => 'Arrer maladie'], 'background' => '#EBF1DE','filter' => ['property_12' => 'medical']],
-			'enterprise' => ['position' => 'I', 'type' => 'count', 'labels' => ['default' => 'Presence en enteprise'], 'background' => '#EBF1DE','filter' => ['property_12' => 'enterprise']],
-			'unjustified' => ['position' => 'J', 'type' => 'count', 'labels' => ['default' => 'Non justifier'], 'background' => '#EBF1DE','filter' => ['property_12' => 'unjustified']],
+			'processing' => ['position' => 'G', 'type' => 'count', 'labels' => ['default' => 'En cours de traitement'], 'background' => '#EBF1DE','filter' => ['property_12' => 'processing']],
+			'medical' => ['position' => 'H', 'type' => 'count', 'labels' => ['default' => 'Arret maladie'], 'background' => '#EBF1DE','filter' => ['property_12' => 'medical']],
+			'enterprise' => ['position' => 'I', 'type' => 'count', 'labels' => ['default' => 'Présence en enteprise'], 'background' => '#EBF1DE','filter' => ['property_12' => 'enterprise']],
+			'unjustified' => ['position' => 'J', 'type' => 'count', 'labels' => ['default' => 'Non justifiée'], 'background' => '#EBF1DE','filter' => ['property_12' => 'unjustified']],
 			'other' => ['position' => 'K', 'type' => 'count', 'labels' => ['default' => 'Autre justificatif'], 'background' => '#EBF1DE','filter' => ['property_12' => 'other']],
 		],
 	],
