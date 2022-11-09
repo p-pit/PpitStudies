3e59d76532079ad0f200a2da11f24dae9ccab116
dylanGema <93646644+dylanGema@users.noreply.github.com>
2022-11-09 17:01:22 +0100

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -5710,42 +5710,6 @@ table.note-report tr.period {
 	
 	// Position Tableau Croisé Excel des absences 
 	'event/report/absence' => [
-		// 'columns' => [
-
-		// 	// Student Details
-		// 	'n_fn' => ['position' => 'A'],
-		// 	'place_caption' => ['position' => 'B'],
-		// 	'email_work' => ['position' => 'C'],
-		// 	'account_groups' => ['position' => 'D'],
-		// 	//'account_property_18' => ['position' => 'E'],
-		// 	// Student Financials
-		// 	'to_justify' => ['position' => 'F', 'type' => 'count', 'labels' => ['default' => 'À justifier'], 'background' => '#EBF1DE', 'filter' => ['property_12' => 'to_justify']],
-		// 	'processing' => ['position' => 'G', 'type' => 'count', 'labels' => ['default' => 'En cour de traitement'], 'background' => '#EBF1DE','filter' => ['property_12' => 'processing']],
-		// 	'medical' => ['position' => 'H', 'type' => 'count', 'labels' => ['default' => 'Arrer maladie'], 'background' => '#EBF1DE','filter' => ['property_12' => 'medical']],
-		// 	'entrepreise' => ['position' => 'I', 'type' => 'count', 'labels' => ['default' => 'Presence en enteprise'], 'background' => '#EBF1DE','filter' => ['property_12' => 'entrepreise']],
-		// 	'unjustified' => ['position' => 'J', 'type' => 'count', 'labels' => ['default' => 'Non justifier'], 'background' => '#EBF1DE','filter' => ['property_12' => 'unjustified']],
-		// 	'other' => ['position' => 'K', 'type' => 'count', 'labels' => ['default' => 'Autre justificatif'], 'background' => '#EBF1DE','filter' => ['property_12' => 'other']],
-
-		// 	'total_student' => [
-		// 		'position' => 'L',
-		// 		'type' => 'computed',
-		// 		'labels' => ['default' => 'Total Etudiant'],
-		// 		'background' => '#EBF1DE',
-		// 		//'indicator' => ['to_justify','processing','medical','unjustified','other'],
-		// 	],
-		// ],
-		// 'sums' => [
-		// 	[
-		// 		// Totals Students
-		// 		'to_justify' => ['position' => 'F', 'type' => 'count', 'labels' => ['default' => 'À justifier'], 'background' => '#EBF1DE', 'filter' => ['property_12' => 'to_justify']],
-		// 		'processing' => ['position' => 'G', 'type' => 'count', 'labels' => ['default' => 'En cour de traitement'], 'background' => '#EBF1DE','filter' => ['property_12' => 'processing']],
-		// 		'medical' => ['position' => 'H', 'type' => 'count', 'labels' => ['default' => 'Arrer maladie'], 'background' => '#EBF1DE','filter' => ['property_12' => 'medical']],
-		// 		'entrepreise' => ['position' => 'I', 'type' => 'count', 'labels' => ['default' => 'Presence en enteprise'], 'background' => '#EBF1DE','filter' => ['property_12' => 'entrepreise']],
-		// 		'unjustified' => ['position' => 'J', 'type' => 'count', 'labels' => ['default' => 'Non justifier'], 'background' => '#EBF1DE','filter' => ['property_12' => 'unjustified']],
-		// 		'other' => ['position' => 'K', 'type' => 'count', 'labels' => ['default' => 'Autre justificatif'], 'background' => '#EBF1DE','filter' => ['property_12' => 'other']],
-		// 	],
-		// ],
-
 		'columns' => [
             // Student Details
             'n_fn' => ['position' => 'A'],
