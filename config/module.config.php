591c9c85f84d72783ae43c8c5e29c64b06dfee37
imaman98 <irfane.maman@groupe-gema.com>
2023-10-11 11:28:39 +0200

    28-191w41-GEMA-Irfane
    masquer onglet inscription  pPitStudies

--- config/module.config.php
+++ config/module.config.php
@@ -6462,7 +6462,7 @@ table.note-report tr.period {
 	'ppitApplications' => array(
 		'p-pit-studies' => array(
 			'labels' => array('fr_FR' => 'P-Pit Studies', 'en_US' => 'Studies by 2Pit'),
-			'default' => 'subscriptions',
+			'default' => 'calendar-next',
 			'defaultRole' => 'teacher',
 		),
 	),
